66b4f27a280f3f45a79784ca2141dfdb8cfa2a29
Karol Horosin <karol.horosin@gmail.com>
2019-05-01 13:10:58 +0100

    Basic template

--- app/samples/urls.py
+++ app/samples/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('other', views.other_page, name='other'),
     path('', views.index, name='index'),
 ]
--- app/samples/views.py
+++ app/samples/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+
+def other_page(request):
+    context = {
+        'test': 'passing a value to the template'
+    }
+    return render(request, 'samples/other.html', context)
