ed314989df45b3d6a146bdfa17b12e409ff86e64
Karol Horosin <karol.horosin@gmail.com>
2019-05-01 17:51:30 +0100

    str method for models

--- app/samples/models.py
+++ app/samples/models.py
@@ -6,9 +6,15 @@ class SampleType(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Sample(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     type = models.ForeignKey(SampleType, on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
