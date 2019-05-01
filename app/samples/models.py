512abde003067c077e8c568f65361e060bfb6ad0
Karol Horosin <karol.horosin@gmail.com>
2019-05-01 17:44:23 +0100

    Basic app models

--- app/samples/models.py
+++ app/samples/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SampleType(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+
+class Sample(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    type = models.ForeignKey(SampleType, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
