f58062a1143efff6e5b17e6db04168608c8fd3ab
Karol Horosin <karol.horosin@gmail.com>
2019-05-06 11:55:45 +0100

    About page - generic template view

--- app/samples/urls.py
+++ app/samples/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -6,5 +8,6 @@ urlpatterns = [
     path('<int:sample_id>/', views.detail, name='detail'),
     path('create/', views.create, name='create'),
     path('create_alt', views.SampleAltCreateView.as_view(), name='create_alt'),
+    path('about/', TemplateView.as_view(template_name="samples/about.html"), name='about'),
     path('', views.index, name='index'),
 ]
